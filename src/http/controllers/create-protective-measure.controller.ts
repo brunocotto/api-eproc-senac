b1d567b5d85b551247a34bc628ecb8ce07240708
Bruno Otto <bruno.ottoo@gmail.com>
2024-06-14 19:50:28 -0300

    feat: base da api eproc gestao agil criada

---
+++ src/http/controllers/create-protective-measure.controller.ts
@@ -0,0 +1,82 @@
+import {
+  BadRequestException,
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+} from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma/prisma.service';
+import { z } from 'zod';
+
+const createProtectiveMeasureBodySchema = z.object({
+  vitimaId: z.number(),
+  agressorId: z.number(),
+  tipoAcao: z.string(),
+  numProcesso: z.number(),
+  numOcorrencia: z.number(),
+  orgaoExpedidor: z.string(),
+  primeiraVisita: z.boolean(),
+  nivelFrida: z.number(),
+  status: z.string(),
+  criadoEm: z.string().refine((date) => !isNaN(Date.parse(date)), {
+    message: 'Invalid date format',
+  }),
+  expiraEm: z.string().refine((date) => !isNaN(Date.parse(date)), {
+    message: 'Invalid date format',
+  }),
+});
+
+type CreateProtectiveMeasureBodySchema = z.infer<
+  typeof createProtectiveMeasureBodySchema
+>;
+
+@Controller('/upload-dados-eproc')
+export class CreateProtectiveMeasureController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  @HttpCode(201)
+  async handle(@Body() body: CreateProtectiveMeasureBodySchema) {
+    const {
+      vitimaId,
+      agressorId,
+      tipoAcao,
+      numProcesso,
+      numOcorrencia,
+      orgaoExpedidor,
+      primeiraVisita,
+      nivelFrida,
+      status,
+      criadoEm,
+      expiraEm,
+    } = body;
+
+    try {
+      const protectiveMeasure = await this.prisma.medidasProtetivas.create({
+        data: {
+          vitimaId,
+          agressorId,
+          tipoAcao,
+          numProcesso,
+          numOcorrencia,
+          orgaoExpedidor,
+          primeiraVisita,
+          nivelFrida,
+          status,
+          criadoEm: new Date(criadoEm),
+          expiraEm: new Date(expiraEm),
+        },
+      });
+
+      return protectiveMeasure;
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new ConflictException('A unique constraint would be violated.');
+      }
+      throw new BadRequestException(
+        'An error occurred while creating the protective measure.',
+      );
+    }
+  }
+}
---
+++ src/http/http.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'src/database/database.module';
+import { CreateProtectiveMeasureController } from './controllers/create-protective-measure.controller';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [CreateProtectiveMeasureController],
+})
+export class HttpModule {}
