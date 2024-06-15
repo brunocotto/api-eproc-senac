f156348fd0fe798801baa512608584bbeb6f79ef
Bruno Otto <bruno.ottoo@gmail.com>
2024-06-14 22:30:55 -0300

    feat: é possível cadastrar um agressor

---
+++ src/http/controllers/create- aggressor.controller.ts
@@ -0,0 +1,87 @@
+import {
+  BadRequestException,
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+} from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma/prisma.service';
+import { z } from 'zod';
+
+const createAggressorBodySchema = z.object({
+  enderecoId: z.number(),
+  nome: z.string().nullable(),
+  nacionalidade: z.string().nullable(),
+  estadoCivil: z.string().nullable(),
+  sexo: z.string().nullable(),
+  cpf: z.string().nullable(),
+  rg: z.number(),
+  telefone: z.string().nullable(),
+  email: z.string().nullable(),
+  profissao: z.string().nullable(),
+  filiacaoMaterna: z.string().nullable(),
+  filiacaoPaterna: z.string().nullable(),
+  dataNascimento: z
+    .string()
+    .nullable()
+    .refine((date) => date === null || !isNaN(Date.parse(date)), {
+      message: 'Invalid date format',
+    }),
+});
+
+type CreateAggressorBodySchema = z.infer<typeof createAggressorBodySchema>;
+
+@Controller('/aggressors')
+export class CreateAggressorController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  @HttpCode(201)
+  async handle(@Body() body: CreateAggressorBodySchema) {
+    const {
+      enderecoId,
+      nome,
+      nacionalidade,
+      estadoCivil,
+      sexo,
+      cpf,
+      rg,
+      telefone,
+      email,
+      profissao,
+      filiacaoMaterna,
+      filiacaoPaterna,
+      dataNascimento,
+    } = body;
+
+    try {
+      const aggressor = await this.prisma.agressor.create({
+        data: {
+          enderecoId,
+          nome,
+          nacionalidade,
+          estadoCivil,
+          sexo,
+          cpf,
+          rg,
+          telefone,
+          email,
+          profissao,
+          filiacao_materna: filiacaoMaterna,
+          filiacao_paterna: filiacaoPaterna,
+          dataNascimento: dataNascimento ? new Date(dataNascimento) : null,
+        },
+      });
+
+      return aggressor;
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new ConflictException('A unique constraint would be violated.');
+      }
+      throw new BadRequestException(
+        'An error occurred while creating the aggressor.',
+      );
+    }
+  }
+}
--- src/http/http.module.ts
+++ src/http/http.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/database/database.module';
 import { CreateProtectiveMeasureController } from './controllers/create-protective-measure.controller';
+import { CreateAggressorController } from './controllers/create- aggressor.controller';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [CreateProtectiveMeasureController],
+  controllers: [CreateProtectiveMeasureController, CreateAggressorController],
 })
 export class HttpModule {}
