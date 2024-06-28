a76542cc8237129b289a8524db94d41684ee6df9
Bruno Otto <bruno.ottoo@gmail.com>
2024-06-28 11:39:36 -0300

    feat: CORS configurado a pedido do grupo 2

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: false,
+    cors: {
+      origin: ['*'],
+      credentials: false,
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+      allowedHeaders: ['Authorization', 'Content-Type'],
+    },
   });
   await app.listen(3000);
 }
